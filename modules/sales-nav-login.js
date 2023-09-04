19c63f8d03880ecabd1fe20491b8a386c4d5d563
milindsoorya <milindsoorya@gmail.com>
2023-09-04 19:40:24 +0100

    sales-nav-scraping

--- modules/sales-nav-login.js
+++ modules/sales-nav-login.js
@@ -6,56 +6,72 @@ async function loginToSalesNavigator(loginUrl, targetUrl, email, password) {
   }
 
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: false, // Run in non-headless mode for debugging
     args: ["--no-sandbox", "--disable-setuid-sandbox", "--start-maximized"],
   });
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1440, height: 900 });
 
   const requestUrls = [];
   const requestHeaders = [];
 
-  await page.setRequestInterception(true);
+  try {
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1440, height: 900 });
 
-  page.on("request", async (request) => {
-    if (request.headers().hasOwnProperty("Cookie")) {
-      const cookie = request.headers()["Cookie"];
-      requestHeaders.push(cookie);
-      requestUrls.push(request.url());
-    }
+    // Set up request interception before navigating to the login page
+    await page.setRequestInterception(true);
 
-    request.continue();
-  });
+    page.on("request", async (request) => {
+      const url = request.url();
+      const targetUrlPattern = "https://www.linkedin.com/sales-api/salesApiLeadSearch";
+      if (url.includes(targetUrlPattern)) {
+        const cookie = request.headers()["cookie"];
+        // console.log("Request cookie:", cookie);
+        console.log("Raw headers:", request.headers());
+        requestHeaders.push(cookie);
+        requestUrls.push(request.url());
+      }
+      request.continue();
+    });
+    
 
-  await page.goto(loginUrl);
-  const iframeElement = await page.$("iframe");
-  const iframe = await iframeElement.contentFrame();
+    await page.goto(loginUrl);
 
-  const EMAIL_SELECTOR = "#username";
-  const PASSWORD_SELECTOR = "#password";
-  const BUTTON_SELECTOR =
-    "#app__container > main > div > form > div.login__form_action_container > button";
+    // Wait for the form input divs to appear
+    await page.waitForSelector(".form__input--floating");
 
-  await iframe.waitForSelector(EMAIL_SELECTOR);
-  await iframe.type(EMAIL_SELECTOR, email);
+    // Find the input elements inside the divs using 'aria-label' attributes
+    const emailInput = await page.$('input[aria-label="Email or Phone"]');
+    await emailInput.type(email);
 
-  await iframe.waitForSelector(PASSWORD_SELECTOR);
-  await iframe.type(PASSWORD_SELECTOR, password);
+    const passwordInput = await page.$('input[aria-label="Password"]');
+    await passwordInput.type(password);
 
-  await iframe.click(BUTTON_SELECTOR);
+    // Find and click the "Sign in" button
+    await page.click('button[aria-label="Sign in"]');
+ 
+    // Add a 2-second delay after successful login
+    await new Promise((resolve) => setTimeout(resolve, 2000));
 
-  await page.waitForNavigation({ waitUntil: "networkidle0" });
+    await page.goto(targetUrl);
 
-  await page.goto(targetUrl, { waitUntil: "networkidle0" });
-
-  setTimeout(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+   
+    // Return results
+    return {
+      status: "success",
+      requestHeaders,
+      requestUrls,
+    };
+  } catch (error) {
+    console.error("Error during the login process:", error.message);
+    return {
+      status: "error",
+      error: error.message,
+    };
+  } finally {
+    // Close the browser when done (whether successful or not)
     await browser.close();
-  }, 10000);
-
-  return {
-    requestHeaders,
-    requestUrls,
-  };
+  }
 }
 
 module.exports = loginToSalesNavigator;
